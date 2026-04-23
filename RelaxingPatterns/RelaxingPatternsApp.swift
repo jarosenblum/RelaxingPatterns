3d99938c9e479e0c57c4d910458c34776397ec45
jarosenblum <jarosenblum@gmail.com>
2026-04-23 17:10:17 -0500

    Initial Commit

---
+++ RelaxingPatterns/RelaxingPatternsApp.swift
@@ -0,0 +1,17 @@
+//
+//  RelaxingPatternsApp.swift
+//  RelaxingPatterns
+//
+//  Created by Jason Rosenblum on 4/23/26.
+//
+
+import SwiftUI
+
+@main
+struct RelaxingPatternsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}

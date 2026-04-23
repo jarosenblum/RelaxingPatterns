2bd7336e9890a8a4760afee7b969875a7a53a347
jarosenblum <jarosenblum@gmail.com>
2026-04-23 18:04:12 -0500

    Add drifting soft particle motion

--- RelaxingPatterns/ContentView.swift
+++ RelaxingPatterns/ContentView.swift
@@ -6,14 +6,17 @@
 //
 
 import SwiftUI
+import Combine
 
 struct SoftCircle: Identifiable {
     let id = UUID()
-    let position: CGPoint
+    var position: CGPoint
     let size: CGFloat
     let color: Color
     let opacity: Double
     let blur: CGFloat
+    let driftX: CGFloat
+    let driftY: CGFloat
 }
 
 struct ContentView: View {
@@ -42,7 +45,7 @@ struct ContentView: View {
             VStack {
                 HStack {
                     Spacer()
-
+                    
                     Button("Clear") {
                         circles.removeAll()
                     }
@@ -53,7 +56,7 @@ struct ContentView: View {
                     .clipShape(Capsule())
                     .padding()
                 }
-
+                
                 Spacer()
             }
         }
@@ -67,7 +70,12 @@ struct ContentView: View {
                     addTrailBurst(at: value.location)
                 }
         )
-
+        .onReceive(
+            Timer.publish(every: 0.08, on: .main, in: .common).autoconnect()
+        ) { _ in
+            moveCircles()
+        }
+        
     }
     
     private func addBurst(at location: CGPoint) {
@@ -76,6 +84,7 @@ struct ContentView: View {
             let jitterY = CGFloat.random(in: -45...45)
             
             return SoftCircle(
+                
                 position: CGPoint(
                     x: location.x + jitterX,
                     y: location.y + jitterY
@@ -83,21 +92,25 @@ struct ContentView: View {
                 size: CGFloat.random(in: 40...150),
                 color: colors.randomElement() ?? .cyan,
                 opacity: Double.random(in: 0.15...0.45),
-                blur: CGFloat.random(in: 6...20)
+                blur: CGFloat.random(in: 6...20),
+                driftX: CGFloat.random(in: -0.8...0.8),
+                driftY: CGFloat.random(in: -0.8...0.8)
             )
         }
         
-        circles.append(contentsOf: newCircles)
+        withAnimation(.easeOut(duration: 0.35)) {
+            circles.append(contentsOf: newCircles)
+        }
         
-        if circles.count > 250 {
-            circles.removeFirst(circles.count - 250)
+        if circles.count > 120 {
+            circles.removeFirst(circles.count - 120)
         }
     }
     private func addTrailBurst(at location: CGPoint) {
         let newCircles = (0..<4).map { _ in
             let jitterX = CGFloat.random(in: -25...25)
             let jitterY = CGFloat.random(in: -25...25)
-
+            
             return SoftCircle(
                 position: CGPoint(
                     x: location.x + jitterX,
@@ -106,14 +119,24 @@ struct ContentView: View {
                 size: CGFloat.random(in: 20...80),
                 color: colors.randomElement() ?? .cyan,
                 opacity: Double.random(in: 0.10...0.35),
-                blur: CGFloat.random(in: 4...14)
+                blur: CGFloat.random(in: 4...14),
+                driftX: CGFloat.random(in: -1.4...1.4),
+                driftY: CGFloat.random(in: -1.4...1.4)
             )
         }
-
-        circles.append(contentsOf: newCircles)
-
+        
+        withAnimation(.easeOut(duration: 0.25)) {
+            circles.append(contentsOf: newCircles)
+        }
+        
         if circles.count > 250 {
             circles.removeFirst(circles.count - 250)
+        }
+    }
+    private func moveCircles() {
+        for index in circles.indices {
+            circles[index].position.x += circles[index].driftX
+            circles[index].position.y += circles[index].driftY
         }
     }
 }
